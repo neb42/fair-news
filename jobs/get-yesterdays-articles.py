f1b497f524cb233cfdda2755096698918e6afc76
Ben McAlindin <bmcalindin@gmail.com>
2018-08-17 07:34:03 +0000

    Store a days worth of articles in one csv file

--- jobs/get-yesterdays-articles.py
+++ jobs/get-yesterdays-articles.py
@@ -7,25 +7,33 @@ if '/project/fair-news' not in sys.path:
     sys.path.append('/project/fair-news')
 
 import tempfile
+import pandas as pd
 import sherlockml.filesystem as sfs
 from datetime import datetime, timedelta
 
 from models.article import Article
 from models.raw_article import RawArticle
 
-from_date = datetime.today().date() - timedelta(days=2)
-to_date = datetime.today().date() - timedelta(days=1)
+# Setup dates
+from_date = datetime.today().date() - timedelta(days=1)
+to_date = datetime.today().date()
 
+# Fetch raw articles
 raw_articles = RawArticle.get_raw_articles(
     from_date.strftime('%Y-%m-%d'),
     to_date.strftime('%Y-%m-%d')
 )
+
+# Build articles and insert into database
 articles = Article.build_articles(raw_articles)
 Article.bulk_insert(articles)
 
-for article in articles:
-    tmp = tempfile.NamedTemporaryFile()
-    with open(tmp.name, 'w') as f:
-        f.write(article.raw_content)
-    sfs.put(tmp.name, f'/input/article_content/{article.article_uuid}')
-    print(article.raw_content)
+# Store raw article content in datasets for later analysis
+df = pd.DataFrame.from_records(
+    [{ 'title': x.title, 'raw_content': x.raw_content, 'article_uuid': x.article_uuid } for x in articles if x.title is not None and x.description is not None]
+)
+tmp = tempfile.NamedTemporaryFile()
+with open(tmp.name, 'w') as f:
+    df.to_csv(tmp.name, sep='\t', encoding='utf-8')
+date_str = from_date.strftime('%Y-%m-%d')
+sfs.put(tmp.name, f'/input/article_content/{date_str}.csv')
