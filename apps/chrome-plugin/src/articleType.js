6a8a86cc56d5d70b716f47e44bfc8283f82c8976
Ben McAlindin <bmcalindin@asidatascience.com>
2019-07-04 18:36:50 +0100

    Get content from page

--- apps/chrome-plugin/src/articleType.js
+++ apps/chrome-plugin/src/articleType.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
+import mimir from 'mimir';
+import Mercury from '@postlight/mercury-parser';
 import * as tf from '@tensorflow/tfjs';
 
+const maxWords = 1000;
 const modelUrl = 'https://storage.googleapis.com/faculty-models/model.json';
 const labelClassesUrl = 'https://storage.googleapis.com/faculty-models/label_classes.json';
+const bagOfWordsUrl = 'https://storage.googleapis.com/faculty-models/bag_of_words.json';
 
 const validCategories = ['politics'];
 
@@ -15,16 +19,37 @@ const loadLabelClasses = async () => {
   return response.data;
 };
 
+const loadBagOfWords = async () => {
+  const response = await axios.get(bagOfWordsUrl);
+  return response.data;
+};
+
 const getPageContent = () => {
-  return '';
+  const { content } = await Mercury.parse(url);
+  return content.replace(/(<([^>]+)>)/ig, '');
+};
+
+const getPageVector = async () => {
+  const content = getPageContent();
+  const bagOfWords = await loadBagOfWords();
+  const vocab = new Array(maxWords).fill('');
+  Object.keys(bagOfWords).forEach(key => {
+    const pos = bagOfWords[key];
+    if (pos < maxWords) {
+      vocab[pos] = key;
+    }
+  });
+  return mimir.bow(content, { words: vocab }).map(v => v === 0 ? 0 : 1);
 };
 
 const getArticleType = async () => {
   const model = await loadModel();
   const labelClasses = await loadLabelClasses();
-  const pageContent = getPageContent();
-  const prediction = model.predict(pageContent);
-  return labelClasses[prediction];
+  const vector = await getPageVector();
+  const predictionsTensor = model.predict(tf.tensor([vector]));
+  const predictions = Array.from(predictionsTensor.dataSync()); 
+  const classIndex = predictions.indexOf(Math.max(...predictions));
+  return labelClasses[classIndex];
 };
 
 export const validateArticleType = async () => {
